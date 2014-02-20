9314e301b2943ed04f412ed98b4111aa7e02eb4e
Rob Clarke <kumo@kumo.it>
2014-02-20 08:59:06 +0100

    moving my bot specific stuff out of library folders

--- roman.js
+++ roman.js
@@ -1,19 +1,43 @@
+//
+//  Roman - Twitter bot that tweets todays date in Roman Numerals format
+//
+// thanks to http://www.apcoder.com/2013/10/03/twitter-bot-20-minutes-node-js/ and http://www.apcoder.com/2013/10/15/targeted-twitter-bots-next-20-minutes/
+
 var moment = require('moment');
 var toRoman = require('roman-numerals').toRoman;
 var util = require('util');
 
-var now = moment();
+var Bot = require('./bot')
+  , config1 = require('./config1');
 
-var day = now.date();
-// var currentLangData = moment.langData();
-// var eng_day = currentLangData.ordinal(day);
-var month = now.month() + 1;
-var year = now.year();
+var bot = new Bot(config1);
 
-var eng_date = moment().format("Do MMMM YYYY");
-var us_date = moment().format("MMMM D YYYY");
+console.log('RomanBot: Running.');
 
-var eng_date_roman = util.format("(%s.%s.%s)", toRoman(day), toRoman(month), toRoman(year));
-var us_date_roman = util.format("(%s.%s.%s)", toRoman(month), toRoman(day), toRoman(year));
+setInterval(function() {
+  var now = moment();
 
-console.log("%s %s / %s %s", eng_date, eng_date_roman, us_date, us_date_roman);
+  var day = now.date();
+  var month = now.month() + 1;
+  var year = now.year();
+
+  var eng_date = moment().format("Do MMMM YYYY");
+  var us_date = moment().format("MMMM D YYYY");
+
+  var eng_date_roman = util.format("(%s.%s.%s)", toRoman(day), toRoman(month), toRoman(year));
+  var us_date_roman = util.format("(%s.%s.%s)", toRoman(month), toRoman(day), toRoman(year));
+
+  var roman_tweet = util.format("%s %s / %s %s", eng_date, eng_date_roman, us_date, us_date_roman);
+  
+  bot.tweet(roman_tweet, function (err, reply) {
+    if(err) return handleError(err);
+
+    console.log('\nTweet: ' + (reply ? reply.text : reply));
+  });
+}, 60 * 60 * 1000 * 24 );
+
+function handleError(err) {
+  console.error('response status:', err.statusCode);
+  console.error('data:', err.data);
+}
+
